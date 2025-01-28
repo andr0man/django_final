6f2703f291fabd6fd28284601855044ebec83d84
andromaan <122400140+andr0man@users.noreply.github.com>
2025-01-28 18:15:13 +0200

    added users and basic layout

--- onlineStore/products/models.py
+++ onlineStore/products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    image = models.ImageField(blank=True, default='product_default.png')
